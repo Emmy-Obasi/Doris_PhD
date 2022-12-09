4a019e3eff8902f9e1bc109f77109a26db04ed70
Emmy-Obasi <108322341+Emmy-Obasi@users.noreply.github.com>
2022-12-09 05:06:48 +0100

    Update index.py

--- Doris/index.py
+++ Doris/index.py
@@ -40,7 +40,7 @@ with dataset:
     st.text('The dataset is customized and cannot be viewed publicly')
 
     #atm = pd.read_csv('atm.csv')
-    atm = pd.read_csv(r'files/atm.csv')
+    atm = pd.read_csv(r'doris/atm.csv')
     st.write(atm.head(5))
     scaler = MinMaxScaler()
 
